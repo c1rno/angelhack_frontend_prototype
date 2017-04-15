9facaf3d6b64105e177daa233135ec659348814c
c1rno <i32311113@icloud.com>
2017-04-15 23:12:19 +0700

    disable unused components and add 2 usefull

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
 	output: {
 		path: util.root('dist'),
-		publicPath: 'http://localhost:4200/',
+		publicPath: 'http://0.0.0.0:4200/',
 		filename: '[name].js',
 		chunkFilename: '[id].chunk.js'
 	},
--- src/demo-app/app/app-module.ts
+++ src/demo-app/app/app-module.ts
@@ -8,36 +8,38 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MdlModule } from './../../lib/components/index';
 import { RouterModule } from '@angular/router';
 import { Angular2MdlAppComponent, Home, appRoutes } from './app.component';
-import { ButtonDemo } from './button/button.component';
-import { BadgeDemo } from './badge/badge.component';
-import { CardDemo } from './card/card.component';
-import { ChipsDemo } from './chips/chips.component';
-import { IconDemo } from './icon/icon.component';
-import { ShadowDemo } from './shadow/shadow.component';
-import { LoadingDemo } from './loading/loading.component';
-import { ListDemo } from './list/list.component';
-import {
-  LayoutDemo,
-  Layout0Demo,
-  Layout1Demo,
-  Layout2Demo,
-  Layout3Demo
-} from './layout/layout.component';
-import { MenuDemo } from './menus/menu.component';
-import { ToggleDemo } from './toggle/toggle.component';
-import { TooltipDemo } from './tooltip/tooltip.component';
-import { SliderDemo } from './slider/slider.component';
-import { SnackbarDemo } from './snackbar/snackbar.component';
-import { TableDemo } from './tables/table.component';
+// import { ButtonDemo } from './button/button.component';
+// import { BadgeDemo } from './badge/badge.component';
+// import { CardDemo } from './card/card.component';
+// import { ChipsDemo } from './chips/chips.component';
+// import { IconDemo } from './icon/icon.component';
+// import { ShadowDemo } from './shadow/shadow.component';
+// import { LoadingDemo } from './loading/loading.component';
+// import { ListDemo } from './list/list.component';
+// import {
+//   LayoutDemo,
+//   Layout0Demo,
+//   Layout1Demo,
+//   Layout2Demo,
+//   Layout3Demo
+// } from './layout/layout.component';
+// import { MenuDemo } from './menus/menu.component';
+// import { ToggleDemo } from './toggle/toggle.component';
+// import { TooltipDemo } from './tooltip/tooltip.component';
+// import { SliderDemo } from './slider/slider.component';
+// import { SnackbarDemo } from './snackbar/snackbar.component';
+// import { TableDemo } from './tables/table.component';
 import { TabsDemo } from './tabs/tabs.component';
-import { TextFieldDemo } from './textfield/textfield.component';
-import { PrismDirective } from './prism/prism.component';
-import { ReactiveFormsDemo } from './reactiveforms/reactiveform.component';
-import { ThemeDemo } from './theme/theme.component';
-import { DialogDemo } from './dialog/dialog.component';
+// import { TextFieldDemo } from './textfield/textfield.component';
+// import { PrismDirective } from './prism/prism.component';
+// import { ReactiveFormsDemo } from './reactiveforms/reactiveform.component';
+// import { ThemeDemo } from './theme/theme.component';
+// import { DialogDemo } from './dialog/dialog.component';
 import { DialogDeclarativeDemo } from './dialog-declarative/dialog-declarative.component';
 import { LoginModule } from './dialog/login.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateQuest } from './create-quest/create-quest.component';
+import { SearchQuest } from './search-quest/search-quest.component';
 
 @NgModule({
   imports: [
@@ -52,32 +54,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     Angular2MdlAppComponent,
     Home,
-    ButtonDemo,
-    BadgeDemo,
-    CardDemo,
-    ChipsDemo,
-    DialogDemo,
+    // ButtonDemo,
+    // BadgeDemo,
+    // CardDemo,
+    // ChipsDemo,
+    // DialogDemo,
     DialogDeclarativeDemo,
-    IconDemo,
-    ShadowDemo,
-    LoadingDemo,
-    ListDemo,
-    LayoutDemo,
-    Layout0Demo,
-    Layout1Demo,
-    Layout2Demo,
-    Layout3Demo,
-    MenuDemo,
-    ToggleDemo,
-    TooltipDemo,
-    SliderDemo,
-    SnackbarDemo,
-    TableDemo,
+    // IconDemo,
+    // ShadowDemo,
+    // LoadingDemo,
+    // ListDemo,
+    // LayoutDemo,
+    // Layout0Demo,
+    // Layout1Demo,
+    // Layout2Demo,
+    // Layout3Demo,
+    // MenuDemo,
+    // ToggleDemo,
+    // TooltipDemo,
+    // SliderDemo,
+    // SnackbarDemo,
+    // TableDemo,
     TabsDemo,
-    TextFieldDemo,
-    ThemeDemo,
-    PrismDirective,
-    ReactiveFormsDemo
+    // TextFieldDemo,
+    // ThemeDemo,
+    // PrismDirective,
+    // ReactiveFormsDemo,
+    CreateQuest,
+    SearchQuest
   ],
   providers: [
     Title
